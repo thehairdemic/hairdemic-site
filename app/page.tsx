be4f4885b7fd9125acc17094fd4530cf70e89b26
thehairdemic <thehairdemic@gmail.com>
2026-08-19 15:38:07 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="appShell">
+      <iframe
+        src="/ecosystem.html"
+        title="My Hairdemic"
+        className="ecosystemFrame"
+        allow="clipboard-read; clipboard-write"
+      />
+    </main>
+  );
+}
